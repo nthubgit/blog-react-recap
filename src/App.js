a7412a304d86bb6eb72c0202a4f60a8c3a179c7d
nthubgit <neltyler@gmail.com>
2024-03-22 13:04:24 -0400

    blog pages, setting up routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import ArticlePage from './pages/ArticlePage';
+import AboutPage from './pages/AboutPage';
+import ArticlesListPage from './pages/ArticlesListPage';
 
 function App() {
   return (
-    <div className="App">
-      <h1>My Awesome Blog</h1>
-      <div id="page-body">
-        Welcome to my blog!
+    <BrowserRouter>
+      <div className="App">
+        <h1>My Awesome Blog</h1>
+        <div id="page-body">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/articles" element={<ArticlesListPage />} />
+          <Route path="/article" element={<ArticlePage />} />
+        </Routes>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
