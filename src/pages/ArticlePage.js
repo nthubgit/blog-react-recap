57a0c3f979a3677d892710c53db3f483bb45ce70
nthubgit <neltyler@gmail.com>
2024-04-04 17:23:49 -0400

    Articles List

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -5,17 +5,17 @@ import { useParams } from "react-router-dom";
 import articles from "./article-content";
 
 const ArticlePage = () => {
-    const { articleId } = useParams();
-    const article = articles.find(article => article.name === articleId);
+  const { articleId } = useParams();
+  const article = articles.find((article) => article.name === articleId);
 
-    return (
-        <>
-        <h1>{article.title}</h1>
-        {article.content.map(paragraph => (
-            <p>{paragraph}</p>
-        ))}
-        </>
-    );
-}
+  return (
+    <>
+      <h1>{article.title}</h1>
+      {article.content.map((paragraph, i) => (
+        <p key={i}>{paragraph}</p>
+      ))}
+    </>
+  );
+};
 
 export default ArticlePage;
