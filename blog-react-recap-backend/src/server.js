595b80f4379cbaee20e0746580a259f69685832a
nthubgit <neltyler@gmail.com>
2024-04-05 16:19:31 -0400

    Testing Postman

--- blog-react-recap-backend/src/server.js
+++ blog-react-recap-backend/src/server.js
@@ -1,10 +1,14 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
 
-app.get("/hello", (req, res) => {
-    res.send("Henlo!")
+app.post("/hello", (req, res) => {
+    console.log(req.body);
+    res.send(`Henlo ${req.body.name}!`)
 });
+
+
 
 app.listen(8000, () => {
     console.log("Server is listening on port 8000.");
