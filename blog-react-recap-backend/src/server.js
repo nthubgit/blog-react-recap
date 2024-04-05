2cbb1a6c2a434740dd6aa844410330d746475ed2
nthubgit <neltyler@gmail.com>
2024-04-05 16:59:06 -0400

    Upvoting articles

--- blog-react-recap-backend/src/server.js
+++ blog-react-recap-backend/src/server.js
@@ -1,18 +1,45 @@
 import express from "express";
 
+let articlesInfo = [
+  {
+    name: "learn-react",
+    upvotes: 0,
+  },
+  {
+    name: "learn-node",
+    upvotes: 0,
+  },
+  {
+    name: "mongodb",
+    upvotes: 0,
+  },
+];
+
 const app = express();
 app.use(express.json());
 
-app.post("/hello", (req, res) => {
-  console.log(req.body);
-  res.send(`Henlo ${req.body.name}!`);
-});
+// app.post("/hello", (req, res) => {
+//   console.log(req.body);
+//   res.send(`Henlo ${req.body.name}!`);
+// });
 
-app.get("/hello/:name", (req, res) => {
-  //const name = req.params.name;
-  console.log(req.params);
-  const { name } = req.params;
-  res.send(`Greetings, ${name}!!`);
+// app.get("/hello/:name", (req, res) => {
+//   //const name = req.params.name;
+//   console.log(req.params);
+//   const { name } = req.params;
+//   res.send(`Greetings, ${name}!!`);
+// });
+
+app.put("/api/articles/:name/upvote", (req, res) => {
+    const { name } = req.params;
+    const article = articlesInfo.find(a => a.name === name);
+    if (article) {
+        article.upvotes += 1;
+        res.send(`The ${name} article now has ${article.upvotes} upvotes.`)
+    }
+    else {
+        res.send("That article doesn\'t exist.")
+    }
 });
 
 app.listen(8000, () => {
