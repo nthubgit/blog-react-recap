c65f9ae72be62a208fd413d3ccd54d8775d62deb
nthubgit <neltyler@gmail.com>
2024-04-05 16:36:32 -0400

    URL params test

--- blog-react-recap-backend/src/server.js
+++ blog-react-recap-backend/src/server.js
@@ -4,12 +4,17 @@ const app = express();
 app.use(express.json());
 
 app.post("/hello", (req, res) => {
-    console.log(req.body);
-    res.send(`Henlo ${req.body.name}!`)
+  console.log(req.body);
+  res.send(`Henlo ${req.body.name}!`);
 });
 
-
+app.get("/hello/:name", (req, res) => {
+  //const name = req.params.name;
+  console.log(req.params);
+  const { name } = req.params;
+  res.send(`Greetings, ${name}!!`);
+});
 
 app.listen(8000, () => {
-    console.log("Server is listening on port 8000.");
+  console.log("Server is listening on port 8000.");
 });
