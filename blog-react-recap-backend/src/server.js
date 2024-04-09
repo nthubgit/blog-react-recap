9f4070cfdc2429f85cade44d783306f168f33cf4
nthubgit <neltyler@gmail.com>
2024-04-09 19:35:36 -0400

    Convert upvotes to MongoDB

--- blog-react-recap-backend/src/server.js
+++ blog-react-recap-backend/src/server.js
@@ -33,10 +33,19 @@ app.get("/api/articles/:name", async (req, res) => {
 //   res.send(`Greetings, ${name}!!`);
 // });
 
-app.put("/api/articles/:name/upvote", (req, res) => {
+app.put("/api/articles/:name/upvote", async (req, res) => {
   const { name } = req.params;
 
-  const article = articlesInfo.find((a) => a.name === name);
+  const client = new MongoClient("mongodb://127.0.0.1:27017");
+  await client.connect();
+
+  const db = client.db("react-blog-db");
+  await db.collection("articles").updateOne({ name }, {
+    $inc: { upvotes: 1},
+  });
+
+  const article = await db.collection('articles').findOne({name});
+
   if (article) {
     article.upvotes += 1;
     res.send(`The ${name} article now has ${article.upvotes} upvotes. Dag yo!`);
